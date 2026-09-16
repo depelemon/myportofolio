7fb8d60cb44a0f6d2ac9b2128143cbe235a4a13e
depelemon <david.liman07@gmail.com>
2026-09-16 22:24:11 +0700

    feat: add music forms and its CRUD endpoints

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import show_main, show_experiences, projects_page, show_music
+from main.views import create_music, delete_music, get_music_json, show_main, show_experiences, projects_page, show_music
 
 app_name = "main"
 
@@ -9,4 +9,7 @@ urlpatterns = [
     path("experiences/", show_experiences, name="show_experiences"),
     path('projects/', projects_page, name='projects_page'),
     path('music/', show_music, name='show_music'),
+    path("music/add/", create_music, name="create_music"),
+    path("music/<uuid:music_id>/delete/", delete_music, name="delete_music"),
+    path("api/music/", get_music_json, name="get_music_json"),
 ]
